b0169a75a110426db17d52686e71c049dd0803a0
Zachary S <zasample18+github@gmail.com>
2023-01-16 00:01:32 -0600

    bitmap: Minor readability & optimization; cargo fmt.

--- bitmap/src/lib.rs
+++ bitmap/src/lib.rs
@@ -24,14 +24,18 @@ impl BitMap {
     }
 
     pub fn get(&self, (row, col): (usize, usize)) -> bool {
-        if row >= self.height || col >= self.width { panic!("index out of range"); }
+        if row >= self.height || col >= self.width {
+            panic!("index out of range");
+        }
         let byte_idx = row * self.stride + col / 8;
         let bit_idx = col % 8;
         (self.data[byte_idx] & (1 << bit_idx)) != 0
     }
 
     pub fn set(&mut self, (row, col): (usize, usize), value: bool) {
-        if row >= self.height || col >= self.width { panic!("index out of range"); }
+        if row >= self.height || col >= self.width {
+            panic!("index out of range");
+        }
         let byte_idx = row * self.stride + col / 8;
         let bit_idx = col % 8;
         let byte = &mut self.data[byte_idx];
@@ -49,7 +53,7 @@ impl BitMap {
     /// Calls `f` with each index whose bit is `true` (row, col)
     pub fn for_each_true(&self, mut f: impl FnMut(usize, usize)) {
         for row in 0..self.height {
-            let byte_range = row * self.stride .. (row + 1) * self.stride;
+            let byte_range = row * self.stride..(row + 1) * self.stride;
             for (byte_col, byte) in self.data[byte_range].iter().enumerate() {
                 for bit_col in 0..8 {
                     let col = (byte_col << 3) | bit_col;
@@ -63,12 +67,15 @@ impl BitMap {
 
     /// Calls `f` with each index whose bit is `false` (row, col)
     pub fn for_each_false(&self, mut f: impl FnMut(usize, usize)) {
-        for row in 0..self.height {
-            let byte_range = row * self.stride .. (row + 1) * self.stride;
-            for (byte_col, byte) in self.data[byte_range].iter().enumerate() {
+        'rows: for row in 0..self.height {
+            let start_byte = row * self.stride;
+            for (byte_col, byte) in self.data[start_byte..][..self.stride].iter().enumerate() {
                 for bit_col in 0..8 {
                     let col = (byte_col << 3) | bit_col;
-                    if byte & (1 << bit_col) == 0 && col < self.width {
+                    if col >= self.width {
+                        continue 'rows;
+                    }
+                    if byte & (1 << bit_col) == 0 {
                         f(row, col);
                     }
                 }
@@ -78,15 +85,18 @@ impl BitMap {
 
     pub fn count(&self) -> usize {
         let mut count = 0;
-        for row in 0..self.height {
-            let byte_range = row * self.stride .. (row + 1) * self.stride;
-            for (byte_col, byte) in self.data[byte_range].iter().enumerate() {
+        'rows: for row in 0..self.height {
+            let start_byte = row * self.stride;
+            for (byte_col, byte) in self.data[start_byte..][..self.stride].iter().enumerate() {
                 if byte_col * 8 + 7 < self.width {
                     count += byte.count_ones() as usize;
                 } else {
                     for bit_col in 0..8 {
                         let col = (byte_col << 3) | bit_col;
-                        if byte & (1 << bit_col) != 0 && col < self.width {
+                        if col >= self.width {
+                            continue 'rows;
+                        }
+                        if byte & (1 << bit_col) != 0 {
                             count += 1;
                         }
                     }
